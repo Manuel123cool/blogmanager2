4132fa19688d66e5989d29e312c8cc8e0595efde
Manuel Kümpel <manuel-kuempel@web.de>
2020-10-15 16:06:44 +0200

    unfinished stuff

--- js/comment.js
+++ js/comment.js
@@ -3,6 +3,45 @@
 let drawCom = {
     drawn: false,
     wrapper: document.getElementById("comment_wrapper"),
+    drawComment: function() {
+        let article = document.createElement('article');        
+
+        let comment_data = document.createElement("div");
+        comment_data.setAttribute("id", "comment_data");
+        article.appendChild(comment_data);
+
+        let name = document.createElement("span");
+        name.innerHTML = document.getElementById("name").value;
+        name.setAttribute("id", "name_span");
+        comment_data.appendChild(name);
+
+        let date = document.createElement("span");
+        let dateObj = new Date();
+        let dateString = dateObj.toDateString(); 
+        date.innerHTML = dateString;
+        date.setAttribute("id", "date_span");
+        comment_data.appendChild(date);
+        
+        let reply = document.createElement("button");
+        reply.setAttribute("id", "reply_button");
+        reply.innerHTML = "reply";
+        comment_data.appendChild(reply);
+       
+        reply.addEventListener("click", replyEvent);
+        article.appendChild(comment_data);
+
+        let p = document.createElement("span");
+        p.setAttribute("id", "text_span");
+        p.innerHTML = document.getElementById("comment").value;
+        article.appendChild(p);
+
+        drawCom.wrapper.appendChild(article);
+        
+        if (!dataCom.replyOn) {
+            let replyIndex1 = drawCom.wrapper.childNodes.length;
+            dataCom.sendData(name.innerHTML, date.innerHTML, p.innerHTML, replyIndex1, 1); 
+        }
+    },
     draw: function(index1, index2) {
         if (!this.drawn) {
             this.drawn = true; 
@@ -32,46 +71,27 @@ let dataCom = {
         });
         xmlhttp0.open('POST', "php/comment.php", true);
         xmlhttp0.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp0.send("name=" + name + "&date=" + date + "&text=" + text + 
+        xmlhttp0.send("name=" + name + "&date=" + date + "&comment=" + text + 
             "&replyIndex=" +  replyIndex1 + "&replyIndex2=" + replyIndex2 +
                 "&siteIndex1=" + this.siteIndex1 + "&siteIndex2=" + this.siteIndex2);
+    },
+    getData: function() {
+        let xmlhttp0 = new XMLHttpRequest();
+        xmlhttp0.addEventListener('readystatechange', (e) => {
+            if (xmlhttp0.readyState==4 && xmlhttp0.status==200) {
+                let responseText = xmlhttp0.responseText;
+            }
+        });
+        xmlhttp0.open('GET', "comment.php?getData=true&siteIndex1=" + this.siteIndex1 +
+                        "&siteIndex2=" + this.siteIndex, true);
+        xmlhttp0.send();  
+ 
     }
 }
 
 function drawCommentEvent(e) {
     e.preventDefault();
-    let article = document.createElement('article');        
-
-    let comment_data = document.createElement("div");
-    comment_data.setAttribute("id", "comment_data");
-    article.appendChild(comment_data);
-
-    let name = document.createElement("span");
-    name.innerHTML = document.getElementById("name").value;
-    name.setAttribute("id", "name_span");
-    comment_data.appendChild(name);
-
-    let date = document.createElement("span");
-    let dateObj = new Date();
-    let dateString = dateObj.toDateString(); 
-    date.innerHTML = dateString;
-    date.setAttribute("id", "date_span");
-    comment_data.appendChild(date);
-    
-    let reply = document.createElement("button");
-    reply.setAttribute("id", "reply_button");
-    reply.innerHTML = "reply";
-    comment_data.appendChild(reply);
-   
-    reply.addEventListener("click", replyEvent);
-    article.appendChild(comment_data);
-
-    let p = document.createElement("span");
-    p.setAttribute("id", "text_span");
-    p.innerHTML = document.getElementById("comment").value;
-    article.appendChild(p);
-
-    drawCom.wrapper.appendChild(article);
+    drawCom.drawComment(); 
 }
     
 function replyEvent(e) {
--- php/comment.php
+++ php/comment.php
@@ -27,21 +27,51 @@ if ($conn->query($sql) === TRUE) {
 }
 $conn->close();
 
+function createTable($index1, $index2) {
+    $sql = "CREATE TABLE IF NOT EXISTS ${index1}comment$index2 (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30),
+        comment VARCHAR(1000),
+        date VARCHAR(30),
+        replyIndex1 INT(30),
+        replyIndex2 INT(30)
+    )";
+
+    if (conn()->query($sql) === TRUE) {
+      echo "Table MyGuests created successfully";
+    } else {
+      echo "Error creating table: " . conn()->error;
+    }
+
+    conn()->close();
+}
+
 function insertData($name, $comment, $date, 
-        $replyIndex1, $replyIndex2, $site_index1, $site_index2) {
-    $sql = "INSERT INTO ${siteIndex1}my_comments$site_index2
+        $replyIndex1, $replyIndex2, $siteIndex1, $siteIndex2) {
+    createTable($siteIndex1, $siteIndex2);
+    $sql = "INSERT INTO ${siteIndex1}comment$siteIndex2
          (comment, name, date, replyIndex1, replyIndex2)
                  VALUES (?, ?, ?, ?, ?)";
     $conn = conn();
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssis", $comment, $name, $date, $reply_num, $into_div);
+    $stmt->bind_param("sssii", $comment, $name, $date, $replyIndex1, $replyIndex2);
     $stmt->execute();
 }
 
+function getData() {
+
+}
+
 if (isset($_POST['name'], $_POST['comment'], $_POST['date'], 
-            $_POST['reply_num'], $_POST['into_div'], $_POST["site_index"])) {
+            $_POST['siteIndex1'], $_POST['siteIndex2'], $_POST["replyIndex"],
+                $_POST["replyIndex2"])) {
 
     insertdata($_POST['name'], $_POST['comment'], $_POST['date'], 
-                    $_POST['reply_num'], $_POST['into_div'], $_POST["site_index"]);    
+            $_POST['replyIndex'], $_POST['replyIndex2'], $_POST["siteIndex1"],
+                $_POST["siteIndex2"]);    
     echo "data arrived";
+}
+
+if (isset($_GET["getData"], $_GET["siteIndex1"], $_GET["siteIndex2"])) {
+    echo "Hallo";
 }
