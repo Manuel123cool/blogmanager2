cc817320d883f55874fe38c8528d82bc13050ad2
Manuel Kümpel <manuel-kuempel@web.de>
2020-10-16 18:19:48 +0200

    comment

--- js/comment.js
+++ js/comment.js
@@ -6,23 +6,23 @@ let drawCom = {
         let article = document.createElement('article');        
 
         let comment_data = document.createElement("div");
-        comment_data.setAttribute("id", "comment_data");
+        comment_data.setAttribute("class", "comment_data");
         article.appendChild(comment_data);
 
         let name = document.createElement("span");
         name.innerHTML = document.getElementById("name").value;
-        name.setAttribute("id", "name_span");
+        name.setAttribute("class", "name_span");
         comment_data.appendChild(name);
 
         let date = document.createElement("span");
         let dateObj = new Date();
         let dateString = dateObj.toDateString(); 
         date.innerHTML = dateString;
-        date.setAttribute("id", "date_span");
+        date.setAttribute("class", "date_span");
         comment_data.appendChild(date);
         
         let reply = document.createElement("button");
-        reply.setAttribute("id", "reply_button");
+        reply.setAttribute("class", "reply_button");
         reply.innerHTML = "reply";
         comment_data.appendChild(reply);
        
@@ -30,7 +30,7 @@ let drawCom = {
         article.appendChild(comment_data);
 
         let p = document.createElement("span");
-        p.setAttribute("id", "text_span");
+        p.setAttribute("class", "text_span");
         p.innerHTML = document.getElementById("comment").value;
         article.appendChild(p);
 
@@ -42,23 +42,24 @@ let drawCom = {
     },
     drawReply: function(referenceNode, name, comment, date) {
         let article = document.createElement('article');        
+        article.setAttribute("class", "deph1");
 
         let comment_data = document.createElement("div");
-        comment_data.setAttribute("id", "comment_data");
+        comment_data.setAttribute("class", "comment_data");
         article.appendChild(comment_data);
 
         let nameElem = document.createElement("span");
         nameElem.innerHTML = name;
-        nameElem.setAttribute("id", "name_span");
+        nameElem.setAttribute("class", "name_span");
         comment_data.appendChild(nameElem);
 
         let dateElem = document.createElement("span");
         dateElem.innerHTML = date;
-        dateElem.setAttribute("id", "date_span");
+        dateElem.setAttribute("class", "date_span");
         comment_data.appendChild(dateElem);
         
         let reply = document.createElement("button");
-        reply.setAttribute("id", "reply_button");
+        reply.setAttribute("class", "reply_button");
         reply.innerHTML = "reply";
         comment_data.appendChild(reply);
        
@@ -66,7 +67,7 @@ let drawCom = {
         article.appendChild(comment_data);
 
         let p = document.createElement("span");
-        p.setAttribute("id", "text_span");
+        p.setAttribute("class", "text_span");
         p.innerHTML = comment;
         article.appendChild(p);
             
@@ -74,6 +75,9 @@ let drawCom = {
             referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
         }
         insertAfter(article, referenceNode);
+    },
+    deleteReply: function(referenceNode) {
+        referenceNode.nextSibling.remove(); 
     },
     drawComments: function(array) {
         //console.log(array);
@@ -85,28 +89,30 @@ let drawCom = {
             let article = document.createElement('article');        
 
             let comment_data = document.createElement("div");
-            comment_data.setAttribute("id", "comment_data");
+            comment_data.setAttribute("class", "comment_data");
             article.appendChild(comment_data);
 
             let name = document.createElement("span");
             name.innerHTML = elem[1];
-            name.setAttribute("id", "name_span");
+            name.setAttribute("class", "name_span");
             comment_data.appendChild(name);
 
             let date = document.createElement("span");
             date.innerHTML = elem[2];
-            date.setAttribute("id", "date_span");
+            date.setAttribute("class", "date_span");
             comment_data.appendChild(date);
             
             let reply = document.createElement("button");
-            reply.setAttribute("id", "reply_button");
+            reply.setAttribute("class", "reply_button");
             reply.innerHTML = "reply";
             comment_data.appendChild(reply);
 
+            let drawOnce = true;
             array.forEach( (elem1) => {
-                if (elem1[3] == count) { 
+                if (elem1[3] == count && drawOnce) { 
+                    drawOnce = false;
                     let showReply = document.createElement("button");
-                    showReply.setAttribute("id", "show_reply");
+                    showReply.setAttribute("class", "show_reply");
                     showReply.innerHTML = "show reply";
                     comment_data.appendChild(showReply);
                     
@@ -118,7 +124,7 @@ let drawCom = {
             article.appendChild(comment_data);
 
             let p = document.createElement("span");
-            p.setAttribute("id", "text_span");
+            p.setAttribute("class", "text_span");
             p.innerHTML = elem[0];
             article.appendChild(p);
 
@@ -136,8 +142,15 @@ let drawCom = {
     }
 }
 
+class ReplyIsOn {
+    constructor(isOn, index) {
+        this.isOn = isOn;
+        this.index = index;
+    }
+}
 let dataCom = {
     array: Array(),
+    showReplyIsOn: Array(),
     replyOn: false,
     replyIndex1: -1,
     replyIndex2: -1,
@@ -185,11 +198,34 @@ function drawCommentEvent(e) {
     
         let dateObj = new Date();
         let dateString = dateObj.toDateString(); 
-        
+            
+        let myDraw = true;
+        dataCom.array.forEach( elem => {
+            if (elem[3] == dataCom.replyIndex1) {
+                myDraw = false;
+            }
+        }); 
+        if (myDraw) {
+            let showReply = document.createElement("button");
+            showReply.setAttribute("class", "show_reply");
+            showReply.innerHTML = "show reply";
+            drawCom.wrapper.childNodes[dataCom.replyIndex1].
+                childNodes[0].appendChild(showReply);
+            showReply.addEventListener("click", showReplyEvent);
+        }
+ 
+        let array = Array(); 
+        array[0] = comment;
+        array[1] = name;
+        array[2] = dateString;
+        array[3] = dataCom.replyIndex1;
+        array[4] = -1;
+        dataCom.array.push(array);
+
         dataCom.sendData(name, dateString, comment, dataCom.replyIndex1, -1);
     }
-}
-    
+} 
+
 function replyEvent(e) {
     e.preventDefault();
       
@@ -220,24 +256,52 @@ function cancelReplyEvent(e) {
 function showReplyEvent(e) {
     let childNodes = drawCom.wrapper.childNodes;
     let count = 0;
+    let count2 = 0;
     let index = 0;
     let indexForChecking = 0;
     childNodes.forEach( (elem) => {
         if (elem == e.currentTarget.parentNode.parentNode) {
             index = count; 
+            indexForChecking = count2; 
         }
-        if (document.querySelector(
+        if (window.getComputedStyle(elem).getPropertyValue("margin-left") != "20px") {
+            count2++; 
+        }
         count++;
     });
+    let deleteReply = false;
+    
+    let showReplyIsOnIsOn = false;
+    let showReplyIsOnElem = false;
+    dataCom.showReplyIsOn.forEach( elem => {
+        if (elem.index == indexForChecking && elem.isOn) {
+            deleteReply = true;
+            elem.isOn = false; 
+        } else if (elem.index == indexForChecking && !elem.isOn) {
+            showReplyIsOnIsOn = true;
+            showReplyIsOnElem = elem;
+        } 
+    });
+    if (!deleteReply) {
+        if (showReplyIsOnIsOn) {
+            showReplyIsOnElem.isOn = true;
+        } else {
+            dataCom.showReplyIsOn.push(new ReplyIsOn(true, indexForChecking)); 
+        }
+    }
     let firstReply = true;
     let count1 = 0;
     dataCom.array.forEach( (elem) => {
-        if (elem[3] == index) {
-            if (firstReply) {
-                drawCom.drawReply(childNodes[index], elem[1], elem[0], elem[2]);
-                firstReply = false;
+        if (elem[3] == indexForChecking) {
+            if (deleteReply) {
+                drawCom.deleteReply(childNodes[index]); 
             } else {
-                drawCom.drawReply(childNodes[index + count1], elem[1], elem[0], elem[2]);
+                if (firstReply) {
+                    drawCom.drawReply(childNodes[index], elem[1], elem[0], elem[2]);
+                    firstReply = false;
+                } else {
+                    drawCom.drawReply(childNodes[index + count1], elem[1], elem[0], elem[2]);
+                }
             }
             count1++;
         }
